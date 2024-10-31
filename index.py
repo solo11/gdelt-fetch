9494ef79eb286373baa67b03f895f4aa82a68026
solo11 <32461868+solo11@users.noreply.github.com>
2024-10-30 21:08:19 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,25 +4,78 @@ import polars as pl
 from azure.cosmos.aio import CosmosClient
 import asyncio
 import os
+import requests
+import extraction
+import pandas as pd
+import requests
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import duckdb
 
 gd2 = gdelt.gdelt(version=2)
 
-
 def get_data():
-    dateStr = date.today()
-    #  - timedelta(1)
-    frmt = "%Y %m %d"
-    dateStr = dateStr.strftime(frmt)
 
-    results = gd2.Search([dateStr], table='gkg', output='pandas dataframe')
+    URL="http://data.gdeltproject.org/gdeltv2/lastupdate.txt"
+    r = requests.get(URL)
 
-    main_df = results[[
-        "DATE", "V2Counts", "SourceCommonName", "DocumentIdentifier",
-        "SharingImage", "V2Themes", "V2Locations", "V2Organizations",
-        "V2Persons"
-    ]].copy().dropna(axis=0)
-    return main_df
+    zip_file = r.text.split('\n')[2].split(' ')[2]
 
+    with urlopen(zip_file) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall('data')
+
+    header = ['GKGRECORDID',
+     'DATE',
+     'SourceCollectionIdentifier',
+     'SourceCommonName',
+     'DocumentIdentifier',
+     'Counts',
+     'V2Counts',
+     'Themes',
+     'V2Themes',
+     'Locations',
+     'V2Locations',
+     'Persons',
+     'V2Persons',
+     'Organizations',
+     'V2Organizations',
+     'V2Tone',
+     'Dates',
+     'GCAM',
+     'SharingImage',
+     'RelatedImages',
+     'SocialImageEmbeds',
+     'SocialVideoEmbeds',
+     'Quotations',
+     'AllNames',
+     'Amounts',
+     'TranslationInfo',
+     'Extras']
+
+    file_name = zip_file.split('/')[-1][:-4]
+    file_path = f'./data/{file_name}'
+
+    try:
+      df = pd.read_csv(file_path,delimiter='\t',names=header)
+      
+      df = df[[
+          "DATE", "V2Counts", "SourceCommonName", "DocumentIdentifier",
+          "SharingImage", "V2Themes", "V2Locations", "V2Organizations",
+          "V2Persons"
+      ]].copy().dropna(axis=0)
+      return(df)
+    except Exception as e:
+      print(f"An unexpected error occurred: {e}")
+      return(None)
+    
+def get_title(url):
+  soup = BeautifulSoup(urlopen(url))
+  title = soup.title.get_text()
+  return(title)
 
 def transform_data(df):
     df_polars = pl.from_pandas(df)
@@ -47,6 +100,7 @@ def transform_data(df):
         pl.struct("DATE", "DocumentIdentifier",
                   'SourceCommonName').hash().cast(
                       pl.String).alias('uuid')).unnest('events')
+    df_data = df_data.with_columns([pl.col('eventDocument').map_elements(get_title,return_dtype=pl.String).alias('title')])
     return df_data
 
 
@@ -84,7 +138,9 @@ async def insert_documents(df, ENDPOINT, credential, DATABASE_NAME,
                 'organizations':
                 row['organizations'],
                 'persons':
-                row['persons']
+                row['persons'],
+                'title':
+                row['title']
             })
 
         print('Upserted Item\'s {0}'.format(rows))
@@ -102,12 +158,38 @@ async def azure_dump(df):
     await asyncio.gather(
         insert_documents(df, ENDPOINT, credential, DATABASE_NAME,
                          CONTAINER_NAME))
-
+def mother_duck_dump(df):
+    con = duckdb.connect(f'md:gdelt_db') 
+    for row in df.rows(named=True):
+        try:
+          title = row['title'].replace("'", "''") if row['title'] else None
+          region = row['region'].replace("'", "''") if row['region'] else None
+          query = f"""INSERT OR IGNORE INTO gdelt
+                    VALUES ({row['uuid']}, 
+                            '{row['date']}',
+                            '{row['sourceName']}',
+                            '{row['eventDocument']}',
+                            '{row['image']}',
+                            '{row['event']}',
+                            {row['eventImportance']},
+                            '{row['latitude']}',
+                            '{row['longitude']}',
+                            '{row['country']}',
+                            '{region}',
+                            {row['organizations']},
+                            {row['persons']},
+                          '{title}');"""
+          con.sql(query)
+        except Exception as e:
+          print(f"An unexpected error occurred: {e}")
+          print(query)
+          break
 
 async def flows():
     df = get_data()
     transform_df = transform_data(df)
     await azure_dump(transform_df)
+    mother_duck_dump(transform_df)
 
 
 async def runs():
